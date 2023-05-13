d9dada7a3aba9e3785e99ced5365f622511ea51b
Joey <54256112+jjaaster@users.noreply.github.com>
2023-05-13 15:57:55 -0400

    Add files via upload

---
+++ Mass and Weight/Mass and Weight/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mass_and_Weight
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int mass = int.Parse(textBox1.Text);
+            double weight = mass * 9.8;
+            textBox2.Text = weight.ToString();
+
+            if (weight > 1000)
+                // Weight Greater than 1000 N
+
+                MessageBox.Show("Mass is too Heavy");
+
+            else if (weight < 10)
+                // Weight Less than 10 N
+                MessageBox.Show("Mass is too Light");
+        }
+    }
+}
