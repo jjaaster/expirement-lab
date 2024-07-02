df28e1fccef748f0166ffc91998c8e20f9e8f2a7
Joseph Jaster <54256112+jjaaster@users.noreply.github.com>
2024-07-02 16:23:02 -0400

    sequential consistency

--- main.go
+++ main.go
@@ -1,32 +1,24 @@
-type processLock struct {
-	mtx      sync.Mutex
-	refcount int
-}
+package main
 
-type locker struct {
-	mtx   sync.Mutex
-	locks map[string]*processLock
-}
+import (
+	"fmt"
+	"sync"
+)
 
-func (l *locker) acquire(key string) {
-	l.mtx.Lock()
-	lk, found := l.locks[key]
-	if !found {
-		lk = &processLock{}
-		l.locks[key] = lk
-	}
-	lk.refcount++
-	l.mtx.Unlock()
-	lk.mtx.Lock()
-}
+var wg sync.WaitGroup
 
-func (l *locker) release(key string) {
-	l.mtx.Lock()
-	lk := l.locks[key]
-	lk.refcount--
-	if lk.refcount == 0 {
-		delete(l.locks, key)
-	}
-	l.mtx.Unlock()
-	lk.mtx.Unlock()
+func main() {
+	wg.Add(2)
+
+	go func() {
+		fmt.Println("sequential consistency 1")
+		wg.Done()
+	}()
+
+	go func() {
+		fmt.Println("sequential consistency 2")
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
